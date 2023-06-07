6aa953acd2c31dee682afb6caaf0317e1f6beb1b
nitishpratap18 <nitish@itinker.io>
2023-06-07 21:14:29 +0530

    changed port

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('./Database/connection')
-const port = 8000
+const port = 8080
 const loginSchema = require('./Models/login')
 app.use(express.json())
 
